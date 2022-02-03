24caefb5a0135789012d7dc19394b4e6b186d5e3
paalss <paalss94@gmail.com>
2022-02-03 18:18:02 +0100

    litt lyst innhold

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +17,25 @@ function App() {
         >
           Learn React
         </a>
+        <p>AAAAA</p>
+        <p
+          style={{ width: "100px", height: "100px", background: "#000" }}
+        ></p>
+        <p
+          style={{ width: "100px", height: "100px", background: "#eee" }}
+        ></p>
+        <p
+          style={{ width: "100px", height: "100px", background: "#000" }}
+        ></p>
+        <p
+          style={{ width: "100px", height: "100px", background: "#eee" }}
+        ></p>
+        <p
+          style={{ width: "100px", height: "100px", background: "#000" }}
+        ></p>
+        <p
+          style={{ width: "100px", height: "100px", background: "#eee" }}
+        ></p>
       </header>
     </div>
   );
